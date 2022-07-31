4b297daafe5482cf2cc07245d045f7e91567e756
Victor Kasap <kasap.victor@gmail.com>
2022-07-31 12:32:31 +0400

    feat: Added hmr

--- app/src/js/index.ts
+++ app/src/js/index.ts
@@ -4,6 +4,14 @@ import '../img/image.jpeg';
 
 import { Button } from './components';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+if (import.meta.webpackHot) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  import.meta?.webpackHot.accept();
+}
+
 Button({ containerName: 'button-wrapper', text: 'Button from JS1', cls: 'button' });
 
 // eslint-disable-next-line no-console
