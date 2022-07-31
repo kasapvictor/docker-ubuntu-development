58979e58b4b77b7d54bc0e83e9d64d47b3d3a276
Victor Kasap <kasap.victor@gmail.com>
2022-07-31 11:56:10 +0400

    feat: Added modules for styles

--- app/src/js/components/Button/Button.ts
+++ app/src/js/components/Button/Button.ts
@@ -1,14 +1,19 @@
 import { ButtonProps } from './types';
 
+import styles from './Button.module.scss';
+
 const buttonHandler = () => {
   // eslint-disable-next-line no-console
   console.log('Click test Button OK');
 };
 
+const buttonClass = styles.button;
+
 const Button = ({ containerName, text, cls = '' }: ButtonProps): void => {
   const button = document.createElement('button');
   button.innerText = text;
   button.classList.add(cls);
+  button.classList.add(buttonClass);
 
   button.addEventListener('click', buttonHandler);
 
--- app/src/js/index.ts
+++ app/src/js/index.ts
@@ -1,8 +1,8 @@
 import '../scss/home.scss';
 import '../img/image2.jpg';
+
 import './common';
 import { foo } from './home';
-
 import { Button } from './components';
 
 Button({ containerName: 'button-wrapper', text: 'Button from JS', cls: 'button' });
